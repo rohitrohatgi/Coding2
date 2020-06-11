d9b1437b0ebc567b8b3ad64f97e961845308e2b4
rohitrohatgi@gmail.com <rohitrohatgi@gmail.com>
2020-06-12 01:30:58 +0800

    commit to git

---
+++ src/main/java/rohit/leetcode/GameOfLifeSolution.java
@@ -0,0 +1,51 @@
+package rohit.leetcode;
+
+/**
+ * Created by rohit on 12/6/20.
+ */
+public class GameOfLifeSolution {
+    public void gameOfLife(int[][] board) {
+        int[] neighbours = {1,0,-1};
+
+        int rows = board.length;
+        int cols = board[0].length;
+
+        for(int row = 0; row < rows ; row++ ){
+            for(int col = 0; col < cols ;  col++){
+                int liveNeighbours = 0;
+                for(int i = 0; i < 3 ; i++){
+                    for(int j = 0; j < 3 ; j++){
+                        if(!(i == 0 && 0 == j)){
+                            int r = row + neighbours[i];
+                            int c = row + neighbours[j];
+                            if(r >= 0 && c >=0 && r < rows && c < cols && Math.abs(board[r][c]) == 1 ){
+                                liveNeighbours++;
+                            }
+                        }
+                    }
+                }
+
+                //add rules
+                if(board[row][col] == 1 &&  (liveNeighbours < 2 || liveNeighbours > 3)){
+                    board[row][col] = -1;
+                }
+                //add rules
+                if(board[row][col] == 0 &&  liveNeighbours == 3){
+                    board[row][col] = 2;
+                }
+
+            }
+        }
+
+        for(int row = 0; row < rows ; row++ ) {
+            for (int col = 0; col < cols; col++) {
+                if (board[row][col] > 0)
+                    board[row][col] = 1;
+                else
+                    board[row][col] = 0;
+            }
+        }
+
+
+    }
+}
