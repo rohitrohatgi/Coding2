a2413715f6859f7270a0df7f880ffb68a09e9305
rohitrohatgi@gmail.com <rohitrohatgi@gmail.com>
2020-06-13 00:17:26 +0800

    commit to git

---
+++ src/main/java/rohit/leetcode/FindLongestWordSolution.java
@@ -0,0 +1,45 @@
+package rohit.leetcode;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by rohit on 12/6/20.
+ */
+public class FindLongestWordSolution {
+    public String findLongestWord(String s, List< String > d) {
+
+        String result = "";
+        Collections.sort(d,(a,b) -> a.length() == b.length()? a.compareTo(b) : Integer.compare(b.length(),a.length()));
+
+        System.out.println(d);
+
+        for(String item : d){
+            if(match(s,item)){
+                return item;
+            }
+        }
+        return result;
+    }
+
+    private boolean match(String s, String item) {
+        int j = 0;
+        for(int i = 0; i < s.length();i++){
+            if(item.charAt(j) == s.charAt(i)) {
+                j++;
+                if(j == item.length()){
+                    break;
+                }
+            }
+        }
+        return (j == item.length());
+    }
+
+    public static void main(String[] args) {
+        FindLongestWordSolution findLongestWordSolution = new FindLongestWordSolution();
+
+        System.out.println(findLongestWordSolution.findLongestWord("abpcplea", Arrays.asList("ale","apple","monkey","plea","aaaa")));
+    }
+}
